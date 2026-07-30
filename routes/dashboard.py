1c3157976b3bb20f5831b0f29fcdb9ceadc1cd6e
Leon <leonnyongesa3@gmail.com>
2026-07-30 23:21:58 +0300

    fix imports

--- routes/dashboard.py
+++ routes/dashboard.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, jsonify
 from flask_jwt_extended import jwt_required, get_jwt_identity
 from sqlalchemy import func
-from app import db
+from extensions import db
 
 from models.transaction import Transaction
 from models.savings_goal import SavingsGoal
